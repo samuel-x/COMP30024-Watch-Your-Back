b7902b42ee72d013175ad25182a1868cfde421a9
Alexander Terp <alexander.terp@gmail.com>
2018-03-14 12:47:07 +1100

    Convert tabs to spaces

--- main.py
+++ main.py
@@ -6,40 +6,40 @@ NUM_ROWS = 8
 NUM_COLS = 8
 
 class Board:
-	'''This represents the board in the game world'''
+    '''This represents the board in the game world'''
 
-	# Board structure
-	board = {}
-	# Move Type
-	moveType = ''
-
-
-	def __init__(self, board):
-		self.board, self.moveType = self.parseBoard(board)
-
-	def parseBoard(self, boardStr):
-		'''Given a string specifying the board layout, converts into a dictionary data structure
-		we can more easily use. Also returns the type of operation required.'''
-		newBoard = {}
-		splitInput = boardStr.split()
-		for rowIndex in range(NUM_ROWS):
-			for colIndex in range(NUM_COLS):
-				newBoard[(rowIndex, colIndex)] = splitInput[rowIndex * NUM_ROWS + colIndex]
-
-		return newBoard, splitInput[-1]
+    # Board structure
+    board = {}
+    # Move Type
+    moveType = ''
 
 
-	def printBoard(self):
-		'''Prints the board'''
-		for rowIndex in range(NUM_ROWS):
-			for colIndex in range(NUM_COLS):
-				print(self.board[(rowIndex, colIndex)], end=' ')
+    def __init__(self, board):
+        self.board, self.moveType = self.parseBoard(board)
 
-			print('')
+    def parseBoard(self, boardStr):
+        '''Given a string specifying the board layout, converts into a dictionary data structure
+        we can more easily use. Also returns the type of operation required.'''
+        newBoard = {}
+        splitInput = boardStr.split()
+        for rowIndex in range(NUM_ROWS):
+            for colIndex in range(NUM_COLS):
+                newBoard[(rowIndex, colIndex)] = splitInput[rowIndex * NUM_ROWS + colIndex]
 
-	def printBoardType(self):
-		'''Prints the type of move'''
-		print(self.moveType)
+        return newBoard, splitInput[-1]
+
+
+    def printBoard(self):
+        '''Prints the board'''
+        for rowIndex in range(NUM_ROWS):
+            for colIndex in range(NUM_COLS):
+                print(self.board[(rowIndex, colIndex)], end=' ')
+
+            print('')
+
+    def printBoardType(self):
+        '''Prints the type of move'''
+        print(self.moveType)
 
 
 # Testing 
