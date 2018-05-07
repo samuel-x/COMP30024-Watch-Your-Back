af31768a19aba2ee0233391c85f2d7953bfc3cfa
Alexander Terp <alexander.terp@gmail.com>
2018-05-07 22:49:05 +1000

    Update Part B's Delta.py

--- Part B/Classes/Delta.py
+++ Part B/Classes/Delta.py
@@ -7,17 +7,19 @@ from Enums.Player import Player
 
 class Delta():
     """
-    A more abstract class. It contains information regarding a move made. An before-board and an appropriate delta
-    should give just enough information to create the resulting board.
+    A more abstract class. It contains information regarding a move made. An
+    before-board and an appropriate delta should give just enough information to
+    create the resulting board.
     """
 
     # The square that the moving piece originated from.
     move_origin: Square
-    # The square that the moving piece moved to. The actual piece object will not be attached to move_target, it will
-    # still be on move_origin.
+    # The square that the moving piece moved to. The actual piece object will
+    # not be attached to move_target, it will still be on move_origin.
     move_target: Square
-    # A list of the positions for squares that had pieces removed at the end of the round. If a player moves a piece to
-    # commit suicide by moving it e.g. between two enemy pieces, the position that the piece ended up on i.e.
+    # A list of the positions for squares that had pieces removed at the end of
+    # the round. If a player moves a piece to commit suicide by moving it e.g.
+    # between two enemy pieces, the position that the piece ended up on i.e.
     # move_target will be included in .killed_square_positions.
     killed_square_positions: List[Pos2D]
     # A list of squares that were eliminated due to the shrinking of the board, not due to the direct movement of an
@@ -38,18 +40,22 @@ class Delta():
         self.eliminated_squares = eliminated_squares
         self.new_corners = new_corners
 
-    def __eq__(self, other: 'Delta'):
+    def __str__(self) -> str:
         """
-        TODO Make this nicer (and for Square?)
-        :param other:
-        :return:
+        Returns a string representation of the calling instance.
         """
-        if (not isinstance(other, Delta)):
-            return False
+        if self.move_origin is None:
+            return "{}".format(self.move_target.pos)
+        else:
+            return "{} -> {}".format(self.move_origin.pos, self.move_target.pos)
 
-        self_tuple = (self.player, self.move_origin, self.move_target, self.killed_square_positions,
-                      self.eliminated_squares, self.new_corners)
-        other_tuple = (other.player, other.move_origin, other.move_target, other.killed_square_positions,
-                           other.eliminated_squares, other.new_corners)
+    def __eq__(self, other: 'Delta') -> bool:
+        """
+        Compares two delta objects.
+        """
+        self_tuple = (self.player, self.move_origin, self.move_target,
+                      self.killed_square_positions)
+        other_tuple = (other.player, other.move_origin, other.move_target,
+                       other.killed_square_positions)
 
         return self_tuple == other_tuple
