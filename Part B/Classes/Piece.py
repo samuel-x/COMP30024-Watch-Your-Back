0cad7b8c030c41d2b2ec03b7fa5ea8fb7dd53dd6
Alexander Terp <alexander.terp@gmail.com>
2018-05-07 22:40:38 +1000

    Update Part B's Piece.py

--- Part B/Classes/Piece.py
+++ Part B/Classes/Piece.py
@@ -6,7 +6,14 @@ class Piece():
     A structure that represents a piece on the board.
     """
 
+    # The player that the piece belongs to.
     owner: Player
+
+    # This is a counter used to uniquely identify every instance of Piece in the
+    # game. This comes into play when a piece jumps over an enemy piece. We want
+    # to ensure that the piece that ends up on the other side of the enemy piece
+    # is not the same from the original position (see implementation of
+    # board._get_killed_positions()).
     _id: int = 0
 
     def __init__(self, owner: Player):
@@ -16,10 +23,14 @@ class Piece():
 
     def getRepresentation(self):
         """
-        Returns the string representation for the piece. Expected use would be for printing the board that the
-        square (which has this piece) is a part of.
+        Returns the string representation for the piece. Expected use would be
+        for printing the board that the square (which has this piece) is a part
+        of.
         """
         return self.owner.getRepresentation()
 
     def __eq__(self, other: 'Piece'):
+        """
+        Compares two piece objects.
+        """
         return self._id == other._id
